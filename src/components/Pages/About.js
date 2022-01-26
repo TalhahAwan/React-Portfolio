0c61e4e92925284422ca3303d3ba40ff44754608
Talhah Awan <talhahawan@gmail.com>
2022-01-25 21:10:55 -0600

    added code into about page

---
+++ src/components/Pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <div class="about-me">
+            <img src='./assets/Portfolio-Image2.jpg' alt='Personal picture' />
+            <h1>About Me</h1>
+            <p>
+            Hi! My name is Talhah Awan. I'm an aspiring software developer who likes to travel to new cities, find new music, all things fashion and photography, and playing rugby. I got into web development becuase I believe its the best mix of creativity and tech. My favorite city i've traveled to is London, i've been going there every year since I was about 2. My favorite type of music is rap with a jazz blend, usually known as acid rap. So far i've loved creating websites, so that should be a good sign, right?
+            </p>
+        </div>
+    );
+}
