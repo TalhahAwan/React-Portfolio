3fd964de6960b6637d2a359d1de2adfeab28c769
Talhah Awan <talhahawan@gmail.com>
2022-01-25 20:46:01 -0600

    working on navigation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,11 @@
-import React, { useState } from 'react';
-import BucketForm from './Footer';
+import React from 'react';
 
+function Navigation ({ currentPage, handlePageChange }) {
+  return (
+    <ul className='nav-tabs'>
+      <li className='nav-item'>
+        <a href='#home' onClick={() => handlePageChange("Home")} className={currentPage === "Home" ? "nav-link active" : "nav-link"}>Home</a>
+      </li>
+    </ul>
+  )
+}
